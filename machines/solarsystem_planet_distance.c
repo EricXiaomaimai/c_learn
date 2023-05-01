292a25a1d47ef3be1c0863aeca6ec794d324540b
Eric <132069567+EricXiaomaimai@users.noreply.github.com>
2023-05-01 18:10:54 +0800

    改进测距程序
    让测距程序可以计算太阳和地球的平均距离（一个天文单位）

--- machines/solarsystem_planet_distance.c
+++ machines/solarsystem_planet_distance.c
@@ -8,38 +8,46 @@ int main() {
     double earth_distance, planet_distance, avg_distance;
 
     // Prompt user to input planet name
-    printf("请输入一个太阳系中的行星（不能是地球）：");
+    printf("请输入一个太阳系中的行星或太阳：");
     scanf("%s", planet);
 
     // Calculate average distance
-    if (strcmp(planet, "水星") == 0) {
-        earth_distance = 0.39;
-        planet_distance = 0.38;
+    if (strcmp(planet, "太阳") == 0) {
+        avg_distance = 1.00;
+    } else if (strcmp(planet, "水星") == 0) {
+        earth_distance = 1.00;
+        planet_distance = 0.39;
+        avg_distance = (earth_distance + planet_distance) / 2.0;
     } else if (strcmp(planet, "金星") == 0) {
-        earth_distance = 0.72;
+        earth_distance = 1.00;
         planet_distance = 0.72;
+        avg_distance = (earth_distance + planet_distance) / 2.0;
     } else if (strcmp(planet, "火星") == 0) {
-        earth_distance = 1.52;
+        earth_distance = 1.00;
         planet_distance = 1.52;
+        avg_distance = (earth_distance + planet_distance) / 2.0;
     } else if (strcmp(planet, "木星") == 0) {
-        earth_distance = 5.20;
+        earth_distance = 1.00;
         planet_distance = 5.20;
+        avg_distance = (earth_distance + planet_distance) / 2.0;
     } else if (strcmp(planet, "土星") == 0) {
-        earth_distance = 9.58;
+        earth_distance = 1.00;
         planet_distance = 9.58;
+        avg_distance = (earth_distance + planet_distance) / 2.0;
     } else if (strcmp(planet, "天王星") == 0) {
-        earth_distance = 19.18;
+        earth_distance = 1.00;
         planet_distance = 19.18;
+        avg_distance = (earth_distance + planet_distance) / 2.0;
     } else if (strcmp(planet, "海王星") == 0) {
-        earth_distance = 30.07;
+        earth_distance = 1.00;
         planet_distance = 30.07;
+        avg_distance = (earth_distance + planet_distance) / 2.0;
     } else {
         printf("输入无效。\n");
         return 1;
     }
 
-    avg_distance = (earth_distance + planet_distance) / 2.0;
-    printf("地球和%s的平均距离为%.2f个天文单位，约为%.2f亿千米。\n",
+    printf("%s和地球的平均距离为%.2f个天文单位，约为%.2f亿千米。\n",
            planet, avg_distance, avg_distance * AU_TO_KM / 1e8);
 
     return 0;
